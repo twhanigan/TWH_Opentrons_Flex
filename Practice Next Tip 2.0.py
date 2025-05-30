ef2017e7e01f4e6b819ce15c9f10d7f5f606b3a2
opatel2426 <om.patel24265@gmail.com>
2025-05-30 15:31:49 -0500

    Update Practice Next Tip 2.0.py
    Changed code
    Used while loop to iterate until available tip is false. Then used search to check for remaining tips.

--- Practice Next Tip 2.0.py
+++ Practice Next Tip 2.0.py
@@ -3,7 +3,7 @@ from opentrons.protocol_api import SINGLE, ALL
 from opentrons import types
 
 metadata = {
-    'protocolName': 'Practice Next Tip',
+    'protocolName': 'Edited Practice Next Tip',
     'author': 'Assistant',
     'description': 'Serial dilution of BSA standard and sample processing. This includes cooling samples to 4c, heating plate to 37c with shaking and recording a video of the whole process. Place BSA Standard in A1, Lysis buffer in A2, change the number of samples and place samples in row B starting at B1. MINIMUM Sample volumen in eppendorf tubes is 40 uL. '
 }
@@ -47,26 +47,43 @@ def run(protocol: protocol_api.ProtocolContext):
                 style=SINGLE,
                 start="A1"
             )
-        if available_tips !=[]:
-            if len(available_tips[0]) >= channels:
-                pip.pick_up_tip(available_tips[0][channels-1])
-                available_tips[0] = available_tips[0][channels:]
+        # if available_tips !=[]:
+        #     if len(available_tips[0]) >= channels:
+        #         pip.pick_up_tip(available_tips[0][channels-1])
+        #         available_tips[0] = available_tips[0][channels:]
+        #     else:
+        #         remaining_tips.append(available_tips[0])
+        #         available_tips.pop(0)
+        #         pip.pick_up_tip(available_tips[0][channels-1])
+        #         available_tips[0] = available_tips[0][channels:]
+        while available_tips:
+            column = available_tips[0]
+            if all(well.has_tip for well in column[:channels]):
+                pip.pick_up_tip(column[0])  # Always pick from top well in column
+                for i in range(channels):
+                    column[i].has_tip = False  # Manually mark as used
+                break
             else:
-                remaining_tips.append(available_tips[0])
-                available_tips.pop(0)
-                pip.pick_up_tip(available_tips[0][channels-1])
-                available_tips[0] = available_tips[0][channels:]
-        
+                remaining_tips.append(available_tips.pop(0))
         else:
-            tip = search_tip(channels)
-            pip.pick_up_tip(tip)
+            # No available columns left, try remaining ones
+            for column in remaining_tips:
+                if all(well.has_tip for well in column[:channels]):
+                    pip.pick_up_tip(column[0])
+                    for i in range(channels):
+                        column[i].has_tip = False
+                    remaining_tips.remove(column)
+                    break
+            else:
+                tip = search_tip(channels)
+                pip.pick_up_tip(tip)
 
     available_tips = partial_50.columns()
     remaining_tips = []
 
-    ch = [5,3,2,7,6,1,8,8,6,7,4,5,3,8,8]
-
+    # ch = [5,3,2,7,6,1,8,8,6,7,4,5,3,8,8]
+    ch = [3,5,8]
     for en, i in enumerate(ch):
-        print(en, i)
+        protocol.comment(f"Run {en+1}: picking {i} channel(s)")
         pick(i)
         p50_multi.drop_tip()
