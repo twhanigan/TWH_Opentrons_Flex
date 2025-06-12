381383bbbf187bd477b548358d3b65de8de54556
opatel2426 <om.patel24265@gmail.com>
2025-06-12 15:27:17 -0500

    Update BCA_with_Normalization_and_Click_Reaction.py

--- BCA_with_Normalization_and_Click_Reaction.py
+++ BCA_with_Normalization_and_Click_Reaction.py
@@ -345,6 +345,7 @@ def run(protocol: protocol_api.ProtocolContext):
     p50_multi.distribute(12, temp_adapter['A5'], [plate3[i] for i in destination_wells], new_tip='always')
 
     # Step 11: shake the sample plate for click reaction
+    protocol.move_labware(labware=plate3, new_location="D1", use_gripper=True)
     heater_shaker.close_labware_latch()
     heater_shaker.set_and_wait_for_shake_speed(400)
     protocol.delay(minutes=60)
